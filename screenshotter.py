0c61b7a41355b01559e0dfd6495dab576b4c49ab
Haskel van Manderpootz <hmanderpootz@gmail.com>
2020-09-03 23:32:00 +0300

    ss

--- screenshotter.py
+++ screenshotter.py
@@ -1,4 +1,14 @@
-import PIL
+from PIL import Image, ImageDraw, ImageFont
 
-def createSS(text, up_img, down_img, upvotes, downvotes):
-    
+up_img = "/home/vanmanderpootz/Desktop/remote_repos/reddit_reader/reddit_reader/postUpvoteIconInactive_n5ydt0uuj6x11.png"
+up = Image.open(up_img)
+
+def createSS(text, upimg):
+    img = Image.new('RGB', (1920, 1080), color = 'rgb(84, 84, 82)')
+    fnt = ImageFont.truetype('/home/vanmanderpootz/Desktop/remote_repos/reddit_reader/reddit_reader/verdana.ttf', 28)
+    d = ImageDraw.Draw(img)
+    d.text((650, 350), text, fill=(255,255,255), font=fnt )
+    img.paste(up, (300, 300))
+    img.save('reddit_bg.png')
+
+createSS("hello world falan filan", up)
