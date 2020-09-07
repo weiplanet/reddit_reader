6c881fb2385dd1cf4e64fb0f9e11b2126aa9a5bf
Muharrem Gurkan CENGIZ <muharrem.cengiz@vizyonkoleji.k12.tr>
2020-09-07 14:29:17 +0300

    improved ss creation and added multiline support

--- reddit_reader.py
+++ reddit_reader.py
@@ -10,11 +10,14 @@ import os
 import screenshotter
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/muharrem.cengiz/Desktop/remote_repos/wikireader-e385429af057.json"
 
-upImage = ""
+upImage = "C:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/postUpvoteIconInactive_n5ydt0uuj6x11.png"
+downImage = "C:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/postDownvoteIconInactive_cnbj1c0wj6x11.png"
+subIcon = "C:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/communityIcon_tijjpyw1qe201bg.png"
+bar = "C:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/bar.png"
 
 folder = input("folder: ")
 
-def text_to_wav(voice_name, text):
+def text_to_wav(voice_name, text, filename):
     language_code = '-'.join(voice_name.split('-')[:2])
     text_input = tts.SynthesisInput(text=text)
     voice_params = tts.VoiceSelectionParams(
@@ -29,7 +32,6 @@ def text_to_wav(voice_name, text):
         voice=voice_params,
         audio_config=audio_config, )
 
-    filename = ("comment{}".format(commentCounter))
     file = pathlib.Path("C:/Users/muharrem.cengiz/Desktop/videolar/YRFP/audio/" + folder + "/" + filename + ".wav")
     with open(file, 'wb') as out:
         out.write(response.audio_content)
@@ -42,6 +44,7 @@ reddit = praw.Reddit(client_id=client_id,
                     password=password,
                     user_agent="reddit_reader")
 
+commentLimit = int(input("Comment amount: "))
 sub_input = input("subreddit: ")
 sub = reddit.subreddit(sub_input)
 sub_hot = sub.hot(limit=9)
@@ -52,17 +55,23 @@ for post in sub_hot:
 
 submission_id = input("id: ")
 submission = reddit.submission(submission_id)
+text_to_wav("en-US-Wavenet-B", submission.title, "post")
+print(" - post text converted to wav - ")
+screenshotter.createPostSS(submission.title, sub_input, subIcon, upImage, downImage, str(submission.ups))
+print(" - created post ss - ")
 submissionList = []
 submission.comments.replace_more(limit=10)
-commentLimit = 10
+
 commentCounter = 0
 for comment in submission.comments.list():
     bod = comment.body
     if commentCounter == commentLimit:
         break
-    submissionList.append(bod.replace("\n", ""))
+    submissionList.append(bod) #.replace("\n", "")
     print(submissionList[commentCounter])
-    text_to_wav("en-US-Wavenet-B", submissionList[commentCounter])
-    screenshotter.createSS(submissionList[commentCounter, upImage])
+    text_to_wav("en-US-Wavenet-B", submissionList[commentCounter], "comment{}".format(commentCounter))
+    screenshotter.reddit_ss = ("comment {}".format(commentCounter))
+    screenshotter.createSS(submissionList[commentCounter], upImage, downImage)
+    print(" - created comment ss -")
     commentCounter += 1
 
--- screenshotter.py
+++ screenshotter.py
@@ -1,12 +1,40 @@
-from PIL import Image, ImageDraw, ImageFont
+from pil import Image, ImageDraw, ImageFont
+import pathlib
+import textwrap
+reddit_ss = "comment"
+post = "post"
+upImage = "C:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/postUpvoteIconInactive_n5ydt0uuj6x11.png"
 
 
-
-def createSS(text, upimg):
+def createSS(text, upimg, downimg):
+    text_str = "".join(text)
+    wrapped = textwrap.fill(text= text_str, width= 100)
     up = Image.open(upimg)
-    img = Image.new('RGB', (1920, 1080), color = 'rgb(84, 84, 82)')
-    fnt = ImageFont.truetype('/home/vanmanderpootz/Desktop/remote_repos/reddit_reader/reddit_reader/verdana.ttf', 28)
+    down = Image.open(downimg)
+    img = Image.new('RGB', (1920, 1080), color = 'rgb(26, 26, 27)')
+    fnt = ImageFont.truetype('c:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/verdana.ttf', 24)
     d = ImageDraw.Draw(img)
-    d.text((650, 350), text, fill=(255,255,255), font=fnt )
-    img.paste(up, (300, 300))
-    img.save('reddit_bg.png')
+    d.multiline_text((350, 320), wrapped, fill=(255,255,255), font=fnt )
+    img.paste(up, (270, 262))
+    img.paste(down, (270, 320))
+    imgdir = pathlib.Path('C:/Users/muharrem.cengiz/Desktop/videolar/YRFP/video1/' + reddit_ss + '.png')
+    img.save(imgdir)
+
+def createPostSS(text, subName, subIcon, upimg, downimg, upvotes):
+    text_str = "".join(text)
+    wrapped = textwrap.fill(text= text_str, width= 100)
+    up = Image.open(upimg)
+    down = Image.open(downimg)
+    icon = Image.open(subIcon)
+    img = Image.new('RGB', (1920, 1080), color = 'rgb(26, 26, 27)')
+    fnt = ImageFont.truetype('c:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/verdana.ttf', 24)
+    upvote_font = ImageFont.truetype('c:/Users/muharrem.cengiz/Desktop/remote_repos/reddit_reader/verdana.ttf', 15)
+    d = ImageDraw.Draw(img)
+    d.multiline_text((350, 320), wrapped, fill=(255,255,255), font=fnt )
+    img.paste(up, (270, 262))
+    img.paste(down, (270, 330))
+    img.paste(icon, (350, 262))
+    d.text((410, 269), "r/{}".format(subName), fill=(255, 255, 255), font=fnt)
+    d.text((280, 310), upvotes, fill=(255, 255, 255), font=upvote_font)
+    imgdir = pathlib.Path('C:/Users/muharrem.cengiz/Desktop/videolar/YRFP/video1/' + post + '.png')
+    img.save(imgdir)
