ad2e1183d7a9a4f5ee2592410fa2bfd2035bfd9e
Muharrem Gurkan CENGIZ <muharrem.cengiz@vizyonkoleji.k12.tr>
2020-09-11 01:45:45 +0300

    tried commiting with vscode

--- video_creator.py
+++ video_creator.py
@@ -5,11 +5,11 @@ from conf import SAMPLE_INPUTS, SAMPLE_OUTPUTS
 
 ss_dir = os.path.join(SAMPLE_INPUTS, "imgs")
 audio_dir = os.path.join(SAMPLE_INPUTS, "audio")
-output = os.path.join(SAMPLE_OUTPUTS, "output.mp4")
+output_video = os.path.join(SAMPLE_OUTPUTS, "output.mp4")
 fps = 0.03
 
 def makeVideo(ssdir, audiodir, outputdir, fps):
     clip = ImageSequenceClip(ssdir, fps)
     clip.write_videofile(outputdir)
 
-makeVideo(ss_dir, audio_dir, output, fps)
+makeVideo(ss_dir, audio_dir, output_video, fps)
